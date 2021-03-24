7a4d63c7bf16e2225209b0bd1cd1d6562ae1efe6
Administrator <Administrator@DESKTOP-2QNS6G3>
2021-03-24 15:56:16 +0800

    添加邮件

---
+++ src/Diomin.Foundation/Diomin.Foundation.Core/Entity/MessageEntity.cs
@@ -0,0 +1,199 @@
+﻿using Foundation.Core.Extensions;
+using Foundation.Core.Utility;
+using System.Collections.Generic;
+
+namespace Foundation.Core.Entity
+{
+    public class MessageEntity
+    {
+        private string smtpServer;
+
+        /// <summary>
+        /// SMTP服务地址
+        /// </summary>
+        public string SmtpServer
+        {
+            get
+            {
+                if (smtpServer == null)
+                    smtpServer = AppConfigManager.Get("Notify:Email:ServerName");
+                return smtpServer;
+            }
+            set { smtpServer = value; }
+        }
+
+        private int serverPort;
+
+        /// <summary>
+        /// 服务端口号
+        /// </summary>
+        public int ServerPort
+        {
+            get
+            {
+                if (serverPort <= 0)
+                    serverPort = int.Parse(AppConfigManager.Get("Notify:Email:serverPort"));
+                return serverPort;
+            }
+            set { serverPort = value; }
+        }
+
+        private string userName;
+
+        /// <summary>
+        /// 邮箱登录用户名
+        /// </summary>
+        public string UserName
+        {
+            get
+            {
+                if (userName == null)
+                    userName = AppConfigManager.Get("Notify:Email:FromName");
+                return userName;
+            }
+            set { userName = value; }
+        }
+
+        private List<string> to;
+
+        /// <summary>
+        /// 接收邮箱集合
+        /// </summary>
+        public List<string> To
+        {
+            get { return to; }
+            set { to = value; }
+        }
+
+        private List<string> cc;
+
+        /// <summary>
+        /// 抄送邮件集合
+        /// </summary>
+        public List<string> Cc
+        {
+            get { return cc; }
+            set { cc = value; }
+        }
+
+        private string subject;
+
+        /// <summary>
+        /// 邮件主题
+        /// </summary>
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        private string body;
+
+        /// <summary>
+        /// 邮件主体
+        /// </summary>
+        public string Body
+        {
+            get { return body; }
+            set { body = value; }
+        }
+
+        private List<string> attachmentList;
+
+        /// <summary>
+        /// 附件列表，需附件的绝对路径
+        /// </summary>
+        public List<string> AttachmentList
+        {
+            get { return attachmentList; }
+            set { attachmentList = value; }
+        }
+
+        private bool cached = true;
+
+        /// <summary>
+        /// 是否缓存邮件（提高程序响应速度）
+        /// </summary>
+        public bool Cached
+        {
+            get { return cached; }
+            set { cached = value; }
+        }
+
+        private bool isHTML = true;
+
+        /// <summary>
+        /// 邮件主体是否为HTML
+        /// </summary>
+        public bool IsHTML
+        {
+            get { return isHTML; }
+            set { isHTML = value; }
+        }
+
+        private string sendPassword;
+
+        /// <summary>
+        /// 发送者密码
+        /// </summary>
+        public string SendPassword
+        {
+            get
+            {
+                if (sendPassword == null)
+                    sendPassword = AppConfigManager.Get("Notify:Email:FromPass");
+                return sendPassword;
+            }
+            set { sendPassword = value; }
+        }
+
+        /// <summary>
+        /// 发送者邮件地址
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                if (email == null)
+                {
+                    email = AppConfigManager.Get("Notify:Email:FromEmail");
+                }
+                return email;
+            }
+            set { email = value; }
+        }
+
+        private string email;
+
+        private string nickName;
+
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName
+        {
+            get
+            {
+                if (nickName == null)
+                    nickName = AppConfigManager.Get("Notify:Email:FromNickName") == null ? AppConfigManager.Get("Notify:Email:FromEmail") : AppConfigManager.Get("Notify:Email:FromNickName");
+                return nickName;
+            }
+            set { email = value; }
+        }
+
+        private bool isEnableSsl;
+
+        public bool IsEnableSsl
+        {
+            get
+            {
+                isEnableSsl = AppConfigManager.Get("Notify:Email:EnableSsl").CastTo(true);
+                return isEnableSsl;
+            }
+            set
+            {
+                isEnableSsl = value;
+            }
+        }
+    }
+}
---
+++ src/Diomin.Foundation/Diomin.Foundation.Core/Utility/MailHelper.cs
@@ -0,0 +1,300 @@
+﻿using Foundation.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Timers;
+
+namespace Foundation.Core.Utility
+{
+    public class MailHelper
+    {
+        #region private static fields
+
+        private static MailHelper instance;
+        private static Timer timer;
+        private static Queue<MessageEntity> msgQueue;
+        private static object lockObject = new object();
+
+        #endregion private static fields
+
+        static MailHelper()
+        {
+            if (instance == null)
+            {
+                instance = new MailHelper();
+            }
+        }
+
+        private MailHelper()
+        {
+        }
+
+        public static void Instance()
+        {
+            if (instance == null)
+            {
+                instance = new MailHelper();
+            }
+        }
+
+        private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+
+            int msgCount = msgQueue.Count;
+            if (msgCount > 0)
+            {
+                while (msgCount-- > 0)
+                {
+                    MessageEntity entity = null;
+                    try
+                    {
+                        entity = msgQueue.Dequeue();
+                        if (entity != null)
+                        {
+                            smtpSend(entity);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        string title = null;
+                        string to = null;
+                        if (entity != null)
+                        {
+                            title = entity.Subject;
+                            to = entity.To.ToString();
+                        }
+                    }
+                }
+            }
+
+            timer.Start();
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string from, string pwd, string to, string subject, string messageText, bool cached)
+        {
+            Send(from, pwd, to, null, null, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string from, string pwd, string to, string cc, string subject, string messageText, bool cached)
+        {
+            Send(from, pwd, to, cc, null, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string to, string subject, string messageText, bool cached)
+        {
+            Send(null, null, to, null, null, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string to, List<string> attachList, string subject, string messageText, bool cached)
+        {
+            Send(null, null, to, null, attachList, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="cc">抄送者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string to, string cc, string subject, string messageText, bool cached)
+        {
+            Send(null, null, to, cc, null, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="cc">抄送者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="attachList">附件集合</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string to, string cc, List<string> attachList, string subject, string messageText, bool cached)
+        {
+            Send(null, null, to, cc, attachList, subject, messageText, true, cached);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="message">消息内容</param>
+        public static void Send(string subject, string message)
+        {
+            var eamil = AppConfigManager.Get("Iduo:Notify:Email:AdminEmail");
+            if (!string.IsNullOrWhiteSpace(eamil))
+            {
+                Send(eamil, subject, message, false);
+            }
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="from">发送方</param>
+        /// <param name="to">接收者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="cc">抄送者地址（可填多个地址，用英文分号“;”分割）</param>
+        /// <param name="attachmentList">邮件附件列表，必须是绝对路径</param>
+        /// <param name="subject">邮件主题</param>
+        /// <param name="messageText">邮件内容</param>
+        /// <param name="cached">是否缓存邮件（提高程序响应速度）</param>
+        public static void Send(string from, string sendPwd, string to, string cc, List<string> attachmentList, string subject, string body, bool isHTML, bool cached)
+        {
+            List<string> TO = null;
+            List<string> CC = null;
+            if (!string.IsNullOrEmpty(to))
+            {
+                string[] arrTo = to.Split(';');
+                TO = new List<string>();
+                for (int i = 0; i < arrTo.Length; i++)
+                {
+                    if (arrTo[i] != null && arrTo[i] != "")
+                    {
+                        TO.Add(arrTo[i]);
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(cc))
+            {
+                string[] arrCC = cc.Split(';');
+                CC = new List<string>();
+                for (int i = 0; i < arrCC.Length; i++)
+                {
+                    if (arrCC[i] != null && arrCC[i] != "")
+                    {
+                        CC.Add(arrCC[i]);
+                    }
+                }
+            }
+            MessageEntity entity = new MessageEntity();
+            entity.AttachmentList = attachmentList;
+            entity.UserName = from;
+            entity.To = TO;
+            entity.Cc = CC;
+            entity.Subject = subject;
+            entity.Body = body;
+            entity.Cached = cached;
+            entity.SendPassword = sendPwd;
+            entity.IsHTML = isHTML;
+            Send(entity);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="entity">邮件配置类</param>
+        public static void Send(MessageEntity entity)
+        {
+            if (entity.Cached)
+            {
+                lock (lockObject)
+                {
+                    if (msgQueue == null)
+                    {
+                        msgQueue = new Queue<MessageEntity>();
+                    }
+                    if (timer == null)
+                    {
+                        timer = new Timer(1000);
+                        timer.Enabled = false;
+                        timer.AutoReset = false;
+                        timer.Elapsed += new ElapsedEventHandler(TimerOnElapsed);
+                    }
+                    if (!timer.Enabled)
+                    {
+                        timer.Enabled = true;
+                    }
+                }
+                lock (msgQueue)
+                {
+                    msgQueue.Enqueue(entity);
+                }
+            }
+            else
+            {
+                smtpSend(entity);
+            }
+        }
+
+        private static void smtpSend(MessageEntity entity)
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient(entity.SmtpServer, entity.ServerPort);
+                smtp.EnableSsl = entity.IsEnableSsl;
+                smtp.Credentials = new NetworkCredential(entity.UserName, entity.SendPassword);
+                MailMessage mm = new MailMessage();
+                //  MailMessage message = new MailMessage(entity.Email, entity.Email, entity.Subject, entity.Body);
+                MailAddress mailAddress = new MailAddress(entity.Email, entity.NickName);
+                mm.From = mailAddress;
+                mm.Body = entity.Body;
+                mm.Subject = entity.Subject;
+                mm.IsBodyHtml = entity.IsHTML;
+                foreach (string to in entity.To)
+                {
+                    if (!string.IsNullOrEmpty(to))
+                        mm.To.Add(to);
+                }
+                if (entity.Cc != null && entity.Cc.Count > 0)
+                {
+                    foreach (string cc in entity.Cc)
+                    {
+                        if (!string.IsNullOrEmpty(cc))
+                            mm.To.Add(new MailAddress(cc, entity.NickName));
+                    }
+                }
+                if (entity.AttachmentList != null && entity.AttachmentList.Count > 0)
+                {
+                    foreach (string attach in entity.AttachmentList)
+                    {
+                        if (!string.IsNullOrEmpty(attach))
+                            if (File.Exists(attach))
+                                mm.Attachments.Add(new Attachment(attach));
+                            else
+                                throw new Exception("附件不存在！FilePath：" + attach);
+                    }
+                }
+
+                smtp.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"邮件发送异常：{ex.Message}");
+            }
+        }
+    }
+}
